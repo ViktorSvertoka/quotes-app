ee87756b516417a1fee3d6297b4b514e89971f41
ViktorSvertoka <victor.svertoka@gmail.com>
2025-07-31 20:09:47 +0300

    feat(files): change structure

--- client/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-start justify-center h-screen">
-      <h2 className="mt-5 text-2xl text-center">Next.js Quotes App</h2>
-    </div>
-  );
-}
